88f7c6db31be30953ca7834c0c644c06ac6e8e56
ith13579 <ith13579@korea.ac.kr>
2020-12-10 23:45:45 +0900

    should 사용법

--- util.spec.js
+++ util.spec.js
@@ -1,8 +1,8 @@
 // spec이 테스트. 테스트가 요구사항의 명세
 // mocha가 대신 실행해줌
 // console.log('start');
-util = require('./utils');
-assert = require('assert');
+const util = require('./utils');
+const should = require('should');
 describe('util.js모듈은', () => {
   it('문자열의 첫번째 문자를 대문자로 변환한다.', () => {
     // given
@@ -10,8 +10,9 @@ describe('util.js모듈은', () => {
     // when
     const result = util.capitalize(testStr);
     // then
-    assert.equal(result, 'Hello');
+    result.should.be.equal('Hello'); // should를 쓰면 가독성이 좋아진다.
   });
 });
 
+// node_modules/.bin/mocha util.spec.js
 // 노드 assert말고 should 라이브러리를 쓰자.
